cb2d2256ee868b86de15a9226b678951cb0c71cf
Vuze <vuze.mhf2resg@gmail.com>
2016-02-27 20:30:30 +0100

    Key update

--- mhef/n3ds.py
+++ mhef/n3ds.py
@@ -160,7 +160,7 @@ class DLCCipher:
 class DLCXCipher:
     def __init__(self, game):
         if game == MHX_JP:
-            self._cipher = Blowfish.new(b'kSKq2aY3W8YwCN3NutWBpv5cIbl3PlRDQDnzdFNHxz3dMBFJywQQSXJ2') # 2016-01-30
+            self._cipher = Blowfish.new(b'zdNy8oVYXigN98vDOpTn6tWtC2MwEdUGls0JzxJ0xeRYgX5jmVHZaZyS') # 2016-02-27
         else:
             raise ValueError('Ivalid game selected.')
 
